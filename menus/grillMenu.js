13d9ab38ec3e00be7b1668872fadf0d4f2d65ceb
corinthdesign <kloptjk@gmail.com>
2019-09-05 16:05:14 -0500

    small syntax fix

--- menus/grillMenu.js
+++ menus/grillMenu.js
@@ -3,7 +3,7 @@ var menu = [
 
 
 
-//Please do not altar any text other than the menu directly below
+//Please do not altar any text other than the menu directly below.
 //In order for each menu item to properly display,
 //please put each item in quotations. ex. "Cheese Pizza"
 //Each item must be followed with a comma, except for the last item
