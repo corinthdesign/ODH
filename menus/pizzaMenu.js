805032152f5d8950427b2c8c3cc63d9ea8a6d7d2
corinthdesign <kloptjk@gmail.com>
2019-09-06 16:06:54 -0500

    Added Multiple Days

--- menus/pizzaMenu.js
+++ menus/pizzaMenu.js
@@ -1,7 +1,8 @@
-var menu = [
+
+//PIZZA MENU FOR ODH
 
 
-
+var menu_monday = [
 
 //Please do not altar any text other than the menu directly below
 //In order for each menu item to properly display,
@@ -9,6 +10,7 @@ var menu = [
 //Each item must be followed with a comma, except for the last item
 
 
+//Monday Lunch and Dinner
 
   "Cheese Pizza",
   "Pepperoni Pizza",
@@ -18,17 +20,133 @@ var menu = [
 
 
 
+]
+
+var menu_tuesday = [
+
+  //Please do not altar any text other than the menu directly below
+  //In order for each menu item to properly display,
+  //please put each item in quotations. ex. "Cheese Pizza"
+  //Each item must be followed with a comma, except for the last item
 
 
+  //Tuesday Lunch and Dinner
 
-
+    "Cheese Pizza",
+    "Pepperoni Pizza",
+    "Sausage Pizza",
+    //Daily Special//
+    "Veggie Pizza"
 
 
 
 ]
 
 
+var menu_wednesday = [
+
+  //Please do not altar any text other than the menu directly below
+  //In order for each menu item to properly display,
+  //please put each item in quotations. ex. "Cheese Pizza"
+  //Each item must be followed with a comma, except for the last item
+
+
+  //Wednesday Lunch and Dinner
+
+    "Cheese Pizza",
+    "Pepperoni Pizza",
+    "Sausage Pizza",
+    //Daily Special//
+    "Veggie Pizza"
+
+
+]
+
+var menu_thursday = [
+
+  //Please do not altar any text other than the menu directly below
+  //In order for each menu item to properly display,
+  //please put each item in quotations. ex. "Cheese Pizza"
+  //Each item must be followed with a comma, except for the last item
+
+
+  //Thursday Lunch and Dinner
+
+    "Cheese Pizza",
+    "Pepperoni Pizza",
+    "Sausage Pizza",
+    //Daily Special//
+    "Veggie Pizza"
+
+
+]
+
+var menu_friday = [
+
+  //Please do not altar any text other than the menu directly below
+  //In order for each menu item to properly display,
+  //please put each item in quotations. ex. "Cheese Pizza"
+  //Each item must be followed with a comma, except for the last item
+
+
+  //Friday Lunch and Dinner
+
+    "Cheese Pizza",
+    "Pepperoni Pizza",
+    "Sausage Pizza",
+    //Daily Special//
+    "Veggie Pizza"
+
+
+]
+
+var menu_saturday = [
+
+  //Please do not altar any text other than the menu directly below
+  //In order for each menu item to properly display,
+  //please put each item in quotations. ex. "Cheese Pizza"
+  //Each item must be followed with a comma, except for the last item
+
+
+  //Saturday Lunch and Dinner
+
+    "Cheese Pizza",
+    "Pepperoni Pizza",
+    "Sausage Pizza",
+    //Daily Special//
+    "Veggie Pizza"
+
+
+
+]
+
+var menu_sunday = [
+
+  //Please do not altar any text other than the menu directly below
+  //In order for each menu item to properly display,
+  //please put each item in quotations. ex. "Cheese Pizza"
+  //Each item must be followed with a comma, except for the last item
+
+
+  //Sunday Lunch and Dinner
+
+    "Cheese Pizza",
+    "Pepperoni Pizza",
+    "Sausage Pizza",
+    //Daily Special//
+    "Veggie Pizza"
+
+
+
+]
+
 function pizzaUpdate() {
-   menu[3] = (document.getElementById("pizzaSpecial").value);
+   menu_monday[3] = (document.getElementById("pizzaSpecial").value);
+   menu_tuesday[3] = (document.getElementById("pizzaSpecial").value);
+   menu_wednesday[3] = (document.getElementById("pizzaSpecial").value);
+   menu_thursday[3] = (document.getElementById("pizzaSpecial").value);
+   menu_friday[3] = (document.getElementById("pizzaSpecial").value);
+   menu_saturday[3] = (document.getElementById("pizzaSpecial").value);
+   menu_sunday[3] = (document.getElementById("pizzaSpecial").value);
    console.log(menu); //to confirm it has been added to the array
 }
