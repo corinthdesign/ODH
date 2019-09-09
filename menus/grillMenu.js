1ebeda9b080d8f97672da8329c9fff2d834aa656
corinthdesign <kloptjk@gmail.com>
2019-09-09 07:31:10 -0500

    Moved grillMenu.js to proper directory

---
+++ menus/grillMenu.js
@@ -0,0 +1,28 @@
+var menu = [
+
+
+
+
+//Please do not altar any text other than the menu directly below.
+//In order for each menu item to properly display,
+//please put each item in quotations. ex. "Cheese Pizza"
+//Each item must be followed with a comma, except for the last item
+
+
+
+  "Cheese Burgers",
+  "Hot Dogs",
+  "Quesadillas",
+  //Daily Special//
+  "Philly Cheese Steaks"
+
+
+
+
+
+
+
+
+
+
+]
