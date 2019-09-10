d5db0b9ef3d606807cd93162aeee5393b1c10093
Tim Klopfenstein <tim@corinthdesign.com>
2019-09-10 07:51:16 -0500

    Updated pizza menu

--- menus/pizzaMenu.js
+++ menus/pizzaMenu.js
@@ -14,7 +14,7 @@ var menu =
 //CHANGING THE DAILY SPECIAL FOR EACH DAY.
 
 "Monday" : "Meat Lover's Pizza",
-"Tuesday" : "Grilled Vegetable Pizza",
+"Tuesday" : "Chicken Pizza",
 "Wednesday" : "Cheeseburger Pizza",
 "Thursday" : "Mac and Cheese Pizza",
 "Friday" : "Tandoori Chicken Pizza",
