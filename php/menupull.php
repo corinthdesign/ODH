673465b42f34bf1cef726035b9e345f666a2e67c
corinthdesign <kloptjk@gmail.com>
2019-09-16 22:48:10 -0500

    simplified echo

--- php/menupull.php
+++ php/menupull.php
@@ -29,7 +29,7 @@ $datafetch = mysqli_query($conn, "select menuItem from pizza");
 while($row=mysqli_fetch_array($datafetch))
 {
 
-echo $row['menuItem']
+echo $row
 
 ;
 //while($result = mysql_fetch_array($datafetch)) {
