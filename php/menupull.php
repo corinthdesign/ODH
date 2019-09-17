9c269c668fe350ab9bf9c9da1667c375bf3e5f3c
corinthdesign <kloptjk@gmail.com>
2019-09-16 21:41:12 -0500

    Attempting to pull menu items

--- php/menupull.php
+++ php/menupull.php
@@ -4,4 +4,21 @@ $username = "tim";
 $password = "THeah71412!";
 $dbname = "menus";
     $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+
+  $data = "SELECT menuItem FROM pizza";
+  $result = $conn->query($data);
+
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+          echo " . $row["dailyitem"].";
+      }
+  } else {
+      echo "0 results";
+  }
+  $conn->close();
 ?>
