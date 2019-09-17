c429c1455cc2f6ffeb87552c1511026c65addedb
corinthdesign <kloptjk@gmail.com>
2019-09-16 22:58:29 -0500

    Reverting to old PHP

--- php/menupull.php
+++ php/menupull.php
@@ -3,25 +3,36 @@ $servername = "localhost";
 $username = "tim";
 $password = "THeah71412!";
 $dbname = "menus";
-
-    // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
 
-    $sql = "SELECT id, firstname, lastname FROM MyGuests";
-    $result = $conn->query($sql);
+//    if ($conn->connect_error) {
+//        die("Connection failed: " . $conn->connect_error);
+//      }
 
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "" . $row["menuItem"]. "<br>";
-        }
-    } else {
-        echo "0 results";
-    }
-    $conn->close();
+//  $data = "SELECT menuItem FROM pizza";
+///  $result = $conn->query($data);
 
+//  if ($result->num_rows > 0) {
+      // output data of each row
+//      while($row = $result->fetch_assoc()) {
+//          echo -" . $row["dailyitem"].";
+//      }
+//  } else {
+//      echo "0 results";
+//  }
+//  $conn->close();
+
+$dataArray = array();
+
+$datafetch = mysqli_query($conn, "select menuItem from pizza");
+
+while($row=mysqli_fetch_array($datafetch))
+{
+
+echo $row["menuItem"]
+
+;
+//while($result = mysql_fetch_array($datafetch)) {
+//    $dataArray[$result['menuItem']] = $result['name'];
+}
 ?>
