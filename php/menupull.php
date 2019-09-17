007efcefff26c3d0ec021073c53deba35ebcfa0b
corinthdesign <kloptjk@gmail.com>
2019-09-16 21:43:09 -0500

    Commenting out additional php

--- php/menupull.php
+++ php/menupull.php
@@ -5,20 +5,20 @@ $password = "THeah71412!";
 $dbname = "menus";
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-      }
+//    if ($conn->connect_error) {
+//        die("Connection failed: " . $conn->connect_error);
+//      }
 
-  $data = "SELECT menuItem FROM pizza";
-  $result = $conn->query($data);
+//  $data = "SELECT menuItem FROM pizza";
+///  $result = $conn->query($data);
 
-  if ($result->num_rows > 0) {
+//  if ($result->num_rows > 0) {
       // output data of each row
-      while($row = $result->fetch_assoc()) {
-          echo " . $row["dailyitem"].";
-      }
-  } else {
-      echo "0 results";
-  }
-  $conn->close();
+//      while($row = $result->fetch_assoc()) {
+//          echo " . $row["dailyitem"].";
+//      }
+//  } else {
+//      echo "0 results";
+//  }
+//  $conn->close();
 ?>
