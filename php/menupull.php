dc6008b4a3ff553a9a5d14dca826c0112466fd7b
corinthdesign <kloptjk@gmail.com>
2019-09-16 21:50:44 -0500

    reworked php code

--- php/menupull.php
+++ php/menupull.php
@@ -15,10 +15,18 @@ $dbname = "menus";
 //  if ($result->num_rows > 0) {
       // output data of each row
 //      while($row = $result->fetch_assoc()) {
-//          echo " . $row["dailyitem"].";
+//          echo -" . $row["dailyitem"].";
 //      }
 //  } else {
 //      echo "0 results";
 //  }
 //  $conn->close();
+
+$dataArray = array();
+
+$datafetch = mysql_query("SELECT menuItem FROM pizza");
+
+while($result = mysql_fetch_array($datafetch)) {
+    $dataArray[$result['menuItem']] = $result['name'];
+}
 ?>
