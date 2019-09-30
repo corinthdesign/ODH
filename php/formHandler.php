f649736b75813e305a74f264fe3df5fdcb6d7bbd
corinthdesign <kloptjk@gmail.com>
2019-09-30 12:50:22 -0500

    update php

--- php/formHandler.php
+++ php/formHandler.php
@@ -29,3 +29,9 @@ if($_POST['submit'] == "Update Menu")
   }
 
 ?>
+
+<?php
+  $fp = fopen('sidebar_subscribers.txt', 'a') or die('fopen failed');
+
+  fwrite($fp, "$name\t$email\t$leader\t$industry\t$country\t$zip\r\n") or die('fwrite failed');
+?>
