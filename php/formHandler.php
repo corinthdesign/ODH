3453fae1b82efd71997b616c164c15cdd80ce697
corinthdesign <kloptjk@gmail.com>
2019-09-30 13:06:53 -0500

    removed php dir writing

--- php/formHandler.php
+++ php/formHandler.php
@@ -35,15 +35,3 @@ if($_POST['submit'] == "Update Menu")
 
   fwrite($fp, "$name\t$email\t$leader\t$industry\t$country\t$zip\r\n") or die('fwrite failed');
 ?>
-
-<?php
- $dir = 'myDir';
-
- // create new directory with 744 permissions if it does not exist yet
- // owner will be the user/group the PHP script is run under
- if ( !file_exists($dir) ) {
-     mkdir ($dir, 0744);
- }
-
- file_put_contents ($dir.'/test.txt', 'Hello File');
- ?>
