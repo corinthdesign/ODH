daa2de52f0c567274d770abd126480328cf19bba
corinthdesign <kloptjk@gmail.com>
2019-09-30 12:47:33 -0500

    update php

--- php/formHandler.php
+++ php/formHandler.php
@@ -1,15 +1,31 @@
 <?php
-if(isset($_POST['day']) && isset($_POST['meal'])) {
-    $data = $_POST['day'] . '-' . $_POST['meal'] . "\r\n";
-    $ret = file_put_contents('mydata.txt', $data, FILE_APPEND | LOCK_EX);
-    if($ret === false) {
-        die('There was an error writing this file');
-    }
-    else {
-        echo "$ret bytes written to file";
-    }
-}
-else {
-   die('no post data to process');
-}
+if($_POST['submit'] == "Update Menu")
+{
+  $errorMessage = "";
+  if(empty($_POST['day']))
+  {
+    $errorMessage .= "<li>Please select which day you are submitting.</li>";
+  }
+  if(empty($_POST['meal']))
+  {
+    $errorMessage .= "<li>Please select with meal you are submitting</li>";
+  }
+    $varDay = $_POST['day'];
+    $varMeal = $_POST['meal'];
+  }
+  if($errorMessage != "")
+  {
+    echo("<p>There was an error:</p>\n");
+    echo("<ul>" . $errorMessage . "</ul>\n");
+  }
+  else
+  {
+    $fs = fopen("menuSubmissions.csv","a");
+    fwrite($fs,$varDay . ", " . $varMeal . "\n");
+    fclose($fs);
+
+    header("Location: ../formSubmit.html");
+    exit;
+  }
+
 ?>
