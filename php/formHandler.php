f9456718b271879ed3f49ce09c524a10da7cc425
corinthdesign <kloptjk@gmail.com>
2019-09-30 10:09:46 -0500

    Updated interface. Added PHP form handler.

---
+++ php/formHandler.php
@@ -0,0 +1,32 @@
+<?php
+if($_POST['submit'] == "Update Menu")
+{
+  $errorMessage = "";
+  if(empty($_POST['day']))
+  {
+    $errorMessage .= "<li>Please select which day you are submitting.</li>";
+  }
+  if(empty($_POST['formName']))
+  {
+    $errorMessage .= "<li>Please select with meal you are submitting</li>";
+  }
+    $varDay = $_POST['day'];
+    $varMeal = $_POST['meal'];
+  }
+  if($errorMessage != "")
+  {
+    echo("<p>There was an error:</p>\n");
+    echo("<ul>" . $errorMessage . "</ul>\n");
+  }
+  else
+  {
+    $fs = fopen("mydata.csv","a");
+    fwrite($fs,$varDay . ", " . $varMeal . "\n");
+    fclose($fs);
+
+    header("Location: formSubmit.html");
+    exit;
+  }
+  ?>
+
+?>
