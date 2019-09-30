589c6388bdc4ae5bbe8b338732c11a7edcce8e26
corinthdesign <kloptjk@gmail.com>
2019-09-30 15:50:13 -0500

    php update

--- php/formHandler.php
+++ php/formHandler.php
@@ -40,7 +40,7 @@ if($_POST['submit'] == "Update Menu")
       }
 
         // Attempt insert query execution
-          $sql = "INSERT INTO menutest SET day = '$varDay', meal = '$varMeal', item = '$varItem'";
+          $sql = "INSERT INTO chefsCorner SET day = '$varDay', meal = '$varMeal', item = '$varItem'";
           if(mysqli_query($link, $sql)){
             echo "Records inserted successfully.";
           } else{
