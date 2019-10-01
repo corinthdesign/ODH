7898fa2502e05820385819536630ffe5e1b381c7
corinthdesign <kloptjk@gmail.com>
2019-10-01 08:02:06 -0500

    php>json/ajax>jquery

---
+++ php/get-data.php
@@ -0,0 +1,30 @@
+<?php
+  // Initialize variable for database credentials
+  $dbhost = 'localhost';
+  $dbuser = 'tim';
+  $dbpass = 'password';
+  $dbname = 'menu';
+
+  //Create database connection
+    $dblink = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+  //Check connection was successful
+    if ($dblink->connect_errno) {
+       printf("Failed to connect to database");
+       exit();
+    }
+
+  //Fetch 3 rows from actor table
+    $result = $dblink->query("SELECT item FROM chefsCorner");
+
+  //Initialize array variable
+    $dbdata = array();
+
+  //Fetch into associative array
+    while ( $row = $result->fetch_assoc())  {
+  	$dbdata[]=$row;
+    }
+
+  //Print array in JSON format
+   echo json_encode($dbdata);
+?>
