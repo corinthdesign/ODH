74b433a93c55b6f35f40ac66b1d6f9a071f44e86
corinthdesign <kloptjk@gmail.com>
2019-10-02 16:26:11 -0500

    php update

--- php/formHandler.php
+++ php/formHandler.php
@@ -14,24 +14,18 @@ if($_POST['submit'] == "Update Menu")
     $varDay = $_POST['day'];
     $varMeal = $_POST['meal'];
     $varChefs = $_POST['chefs'];
-    $varMain = $_POST['main'];
-    $varVeg = $_POST['veg'];
-    $varPiz = $_POST['piz'];
-    $varGrill = $_POST['grill'];
-    $varDeli = $_POST['deli'];
-    $varSoup = $_POST['soup'];
-    $varSoup2 = $_POST['soup2'];
+
   }
   if($errorMessage != "")
   {
     echo("<p>There was an error:</p>\n");
     echo("<ul>" . $errorMessage . "</ul>\n");
   }
-//Write Data to file/mySQLserver Section
+//Write Data to php/datalog.txt
   else
   {
     $fs = fopen("data/datalog.txt","a");
-    fwrite($fs,$varDay . ", " . $varMeal . ", " . $varChefs . ", " . $varMain . ", " . $varVeg . ", " . $varPiz . ", " . $varGrill . ", " . $varDeli . "\n");
+    fwrite($fs,$varDay . ", " . $varMeal . ", " . $varChefs . "\n");
     fclose($fs);
 
 //mySQLserver connection
@@ -43,26 +37,13 @@ if($_POST['submit'] == "Update Menu")
       }
 
         // Attempt insert query execution
-          $sql1 = "UPDATE daily SET chefs = '$varChefs' WHERE day = '$varDay' AND meal = '$varMeal'";
-          $sql2 = "UPDATE daily SET main = '$varMain' WHERE day = '$varDay' AND meal = '$varMeal'";
-          $sql3 = "UPDATE daily SET veg = '$varVeg' WHERE day = '$varDay' AND meal = '$varMeal'";
-          $sql4 = "UPDATE daily SET piz = '$varPiz' WHERE day = '$varDay' AND meal = '$varMeal'";
-          $sql5 = "UPDATE daily SET grill = '$varGrill' WHERE day = '$varDay' AND meal = '$varMeal'";
-          $sql6 = "UPDATE daily SET deli = '$varDeli' WHERE day = '$varDay' AND meal = '$varMeal'";
-          $sql7 = "UPDATE daily SET soup = '$varSoup' WHERE day = '$varDay' AND meal = '$varMeal'";
-          $sql8 = "UPDATE daily SET soup2 = '$varSoup2' WHERE day = '$varDay' AND meal = '$varMeal'";
+          $sql = "UPDATE daily SET chefs = '$varChefs' WHERE day = '$varDay' AND meal = '$varMeal'";
 
-          if(mysqli_multi_query($link, $sql1, $sql2, $sql3, $sql4, $sql5, $sql6, $sql7, $sql8)){
+          if(mysqli_multi_query($link, $sql){
             echo "Records inserted successfully.";
           } else{
-            echo "ERROR: Could not execute $sql1. " . mysqli_error($link);
-            echo "ERROR: Could not execute $sql2. " . mysqli_error($link);
-            echo "ERROR: Could not execute $sql3. " . mysqli_error($link);
-            echo "ERROR: Could not execute $sql4. " . mysqli_error($link);
-            echo "ERROR: Could not execute $sql5. " . mysqli_error($link);
-            echo "ERROR: Could not execute $sql6. " . mysqli_error($link);
-            echo "ERROR: Could not execute $sql7. " . mysqli_error($link);
-            echo "ERROR: Could not execute $sql8. " . mysqli_error($link);
+            echo "ERROR: Could not execute $sql. " . mysqli_error($link);
+
           }
 
     // Close connection
