43ceefb5af0547fa2cc25f1614f513449f22f519
corinthdesign <kloptjk@gmail.com>
2019-10-17 13:43:06 -0500

    test

--- test/handler.php
+++ test/handler.php
@@ -1,6 +1,9 @@
 <?php
 
 $var1 = $_POST['data'];
+$var2 = $_POST['datatwo'];
+$var3 = $_POST['datathree'];
+$var4 = $_POST['datafour'];
 
 //mySQLserver connection
     $link = mysqli_connect("localhost", "tim", "password", "test");
@@ -12,6 +15,9 @@ $var1 = $_POST['data'];
 
         // Attempt insert query execution
           $sql = "UPDATE test SET Name = '$var1' WHERE id = '1'";
+          $sql = "UPDATE test SET Name = '$var2' WHERE id = '2'";
+          $sql = "UPDATE test SET Name = '$var3' WHERE id = '3'";
+          $sql = "UPDATE test SET Name = '$var4' WHERE id = '4'";
 
                   if(mysqli_multi_query($link, $sql)){
                     echo "Records inserted successfully.";
