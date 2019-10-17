33573f4bc47497ec65a7735670de5540c5597540
corinthdesign <kloptjk@gmail.com>
2019-10-17 12:47:47 -0500

    update test

--- test/handler.php
+++ test/handler.php
@@ -23,9 +23,9 @@ if($_POST['submit'] == "Update Menu")
       }
 
         // Attempt insert query execution
-          $sql = "UPDATE test SET Name = '$var1' WHERE id = 1;
-                  UPDATE test SET Name = '$var2' WHERE id = 2;
-                  UPDATE test SET Name = '$var3' WHERE id = 3";
+          $sql = "UPDATE test SET Name = '$var1' WHERE id = '1';
+                  UPDATE test SET Name = '$var2' WHERE id = '2';
+                  UPDATE test SET Name = '$var3' WHERE id = '3'";
 
                   if(mysqli_multi_query($link, $sql)){
                     echo "Records inserted successfully.";
