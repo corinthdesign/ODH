b9e11588a11d5ea0d69bbb5cd229126e1dfc7804
corinthdesign <kloptjk@gmail.com>
2020-01-06 13:02:09 -0600

    Reforming site structure

--- node/app/pizza.js
+++
@@ -1,8 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-let App = require('./components/Pizza.js');
-
-ReactDOM.render(
-   <App />,
-   document.getElementByID('app')
-);
